0789357974451e595fa148cc951d651b89c54f50
Tim McIver <tim@timmciver.com>
2019-08-27 00:07:09 -0400

    Importing Data.RDF.

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,11 @@ module Main where
 
 import System.Environment
 import Graphics.HsExif
+import Data.RDF
+import Data.Map as Map
+
+exifToRdf :: Map.Map ExifTag ExifValue -> RDF TList
+exifToRdf = undefined
 
 main :: IO ()
 main = do
