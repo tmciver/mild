7ecde8e88e4239291f59308d00e174ce505a72bf
Tim McIver <tim@timmciver.com>
2020-06-06 16:23:05 -0400

    Pretty print exif data and remove `makerNote` (too long).

--- app/Main.hs
+++ app/Main.hs
@@ -1,17 +1,33 @@
 module Main where
 
-import System.Environment
-import Graphics.HsExif
-import Data.RDF
+import Data.Coerce
+import Data.List (intercalate)
 import Data.Map as Map
+import Data.RDF
+import Data.Set as Set
+import Graphics.HsExif
+import System.Environment
+
+newtype ExifData = ExifData (Map.Map ExifTag ExifValue)
+
+instance Show ExifData where
+  show (ExifData edm) = intercalate "\n" $ show <$> Map.toList edm
 
 exifToRdf :: Map.Map ExifTag ExifValue -> RDF TList
 exifToRdf = undefined
+
+removeMakerNote :: ExifData -> ExifData
+removeMakerNote = coerce . removeMakerNoteMap . coerce
+ where removeMakerNoteMap :: Map.Map ExifTag ExifValue -> Map.Map ExifTag ExifValue
+       removeMakerNoteMap = flip Map.withoutKeys (Set.singleton makerNote)
 
 main :: IO ()
 main = do
   args <- getArgs
   let filePath = head args
   eitherExif <- parseFileExif filePath
-  putStrLn (show eitherExif)
+  let eitherExifData = removeMakerNote . ExifData <$> eitherExif
+  --let eitherTags = Map.keys <$> eitherExif
+  --putStrLn (show eitherTags)
+  putStrLn (show eitherExifData)
   return ()
