ce571bc18ef5bc2c7b4e24870d1e877e18e625e6
Tim McIver <tim@timmciver.com>
2020-06-06 21:03:02 -0400

    Added doc to `removeMakerNote` function.

--- app/Main.hs
+++ app/Main.hs
@@ -16,6 +16,9 @@ instance Show ExifData where
 exifToRdf :: Map.Map ExifTag ExifValue -> RDF TList
 exifToRdf = undefined
 
+-- |Remove the `makerNote` EXIF property from the given 'ExifData'.  This is
+-- typically done because the value of this property is a long string of what
+-- appears to be garbage.
 removeMakerNote :: ExifData -> ExifData
 removeMakerNote = coerce . removeMakerNoteMap . coerce
  where removeMakerNoteMap :: Map.Map ExifTag ExifValue -> Map.Map ExifTag ExifValue
